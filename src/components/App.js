479345bab7019d298616088acf11b259cb067537
EstrellaBF <stern.von.nord>
2018-03-09 22:48:14 -0500

    realizando componentes de songlist, mejorando archivos y carpetas

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import SongList from './SongList'
+
+// const todos = [
+//   { id: 1, text: '', completed: false },
+//   { id: 2, text: '', completed: true },
+//   { id: 3, text: '', completed: false },
+//   { id: 4, text: '', completed: false },
+//   { id: 5, text: '', completed: true },
+//   { id: 6, text: '', completed: true },
+//   { id: 7, text: '', completed: true },
+//   { id: 8, text: '', completed: true },
+//   { id: 9, text: '', completed: true },
+// ]
+
+const App = () => (
+  <div>
+
+    <SongList />
+  </div>
+)
+
+export default App
