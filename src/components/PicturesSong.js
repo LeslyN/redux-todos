e032c0b366fa7a5d9fcc5779f4450e38f6241478
EstrellaBF <stern.von.nord>
2018-03-10 14:57:20 -0500

    añadiendo imagenes

--- src/components/PicturesSong.js
+++ src/components/PicturesSong.js
@@ -1,25 +1,27 @@
 import React from 'react'
 
-const route = '../../public/assets/images/'
-const images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg',]
+const route = 'assets/images/'
+const images = [{url:'1.jpg'}, {url:'2.jpg'}, {url:'3.jpg'}, {url:'4.jpg'}, {url:'5.jpg'}];
 
-const ImagesArr = () => {
-  return (
-    <div>
-      <img src={route +(images.map((val, index) => {return route+val})) } alt={'alt'}/>
-    </div>
-  );
-}
+
+// const ImagesArr = () => {
+
+// }
 
 const PicturesSong = () => {
+  // let bla = images.map((val, index)=>{return <img src={val.url} alt={'hola'}/>});
+  // let bla = images.map((val, index)=>{return (val)});
+    let bla = images.map((val, index)=>{return <img src={route + val.url} alt='hola'/>});
+  // return (
+  //     // <img src={route + bla[1].url} alt={'alt'}/>
+  // );
   return (
     <div>
       <button></button>
-        <ImagesArr />
+        {bla}
       <button></button>
     </div>
   );
 }
-
 
 export default PicturesSong;
