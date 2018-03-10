3497867fd287a4ec54a38ec033da1ce4a2d2dc47
LeslyN <lnombertoc@gmail.com>
2018-03-10 12:19:55 -0500

    cambios

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SongList from './SongList'
+import PicturesSong from './PicturesSong'
+import RankingBotons from './RankingBotons'
+
+const App = () => (
+  <div>
+    <PicturesSong />
+    <SongList />
+    <RankingBotons />
+  </div>
+)
+
+export default App
---
+++ src/components/RankingBotons.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './RankingBotons.css'
+
+const operationsSongs = [
+  { id: 0, nameButton: '+' },
+  { id: 1, nameButton: '-' },
+];
+
+
+const Ranking = () => {
+  return (
+    <div className="clase1">
+      { playList.map((item, index) => {
+        return <p key={index} id={index}>{item.nameButton}</p>
+        })
+      }
+    <button
+      style={{ marginLeft: 10 }}
+    >
+      <b>x</b>
+    </button>
+    </div>
+  )                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+}
+
+export default RankigBotons
