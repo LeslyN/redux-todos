99e966a9c937f75988e16df5e7b19b29e57f8947
EstrellaBF <stern.von.nord>
2018-03-10 21:16:51 -0500

    poniendo estilos

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,11 @@ import SongList from './SongList'
 import PicturesSong from './PicturesSong'
 // import RankingButtons from './RankingButtons'
 
-import TodoList from './Prueba.js'
-
 const App = () => (
   <div>
     <PicturesSong />
     <SongList />
     {/* <RankingButtons /> */}
-    <TodoList/>
   </div>
 )
 
---
+++ src/components/Points.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+// Aquí se visualizará el ranking, se pone en div para visualizarlo mejor
+const Points = () => {
+  return(
+    <button className="check-button" style={{ marginLeft: 10 }}><b>x</b></button>
+  )  
+}
+
+
+export default Points;
