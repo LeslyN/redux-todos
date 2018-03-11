ff8906d285dd256d65cf46e17ccee7d375d6a76e
EstrellaBF <stern.von.nord>
2018-03-10 20:31:36 -0500

    actualizando

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import SongList from './SongList'
 import PicturesSong from './PicturesSong'
-import RankingButtons from './RankingButtons'
+// import RankingButtons from './RankingButtons'
+
+import TodoList from './Prueba.js'
 
 const App = () => (
   <div>
     <PicturesSong />
     <SongList />
     {/* <RankingButtons /> */}
+    <TodoList/>
   </div>
 )
 
